d1c30ce26f3fbb8fe7d4b792f66c070ebe8206cd
engineer-br0 <106507655+engineer-br0@users.noreply.github.com>
2023-05-05 00:45:02 +0530

    completed

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score 
 
 dataset = pd.read_csv("./diabetes.csv")
 
@@ -10,7 +14,36 @@ dataset = pd.read_csv("./diabetes.csv")
 
 X = dataset.drop(columns='Outcome')
 Y = dataset['Outcome']
-standerized_data = StandardScaler().fit_transform(X)
-X = standerized_data
-print(X)
 
+scaler = StandardScaler()
+scaler.fit(X)
+standerized_data = scaler.transform(X)
+X = standerized_data
+#print(X)
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, shuffle=True, test_size=0.15, random_state=100, stratify=Y)
+#print(Y_test.head)
+
+classifier = svm.SVC(kernel='linear')
+
+classifier.fit(X_train, Y_train)
+
+X_train_prdiction = classifier.predict(X_train)
+X_test_prediction = classifier.predict(X_test)
+# print("pred", X_train_prdiction)
+# print("test", Y_test)
+
+X_train_accuracy = accuracy_score(X_train_prdiction, Y_train)
+X_test_accuracy = accuracy_score(X_test_prediction, Y_test)
+# print("X_train_accuracy", X_train_accuracy)
+# print("x_test_accuracy", X_test_accuracy)
+
+#---------------
+input = (8,183,64,0,0,23.3,0.672,32)
+input_numpy = np.asarray(input)
+input_numpy = input_numpy.reshape(1,-1)
+input = scaler.transform(input_numpy)
+
+input_prediction = classifier.predict(input)
+
+print(input_prediction)
